4380f5800289c5701155bcee1ead17ba0b3b2f72
HARU <chu_ba@naver.com>
2023-05-25 14:56:44 +0900

    add model, route

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from  "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "../Booking App_Practice/routes/auth.js"
+import hotelsRoute from "../Booking App_Practice/routes/hotels.js"
+import roomsRoute from "../Booking App_Practice/routes/rooms.js"
+import usersRoute from "../Booking App_Practice/routes/users.js"
+
+
+const app = express()
+dotenv.config()
+
+const connect = async ()=> {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.")
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected!")
+})
+
+
+//middleware
+
+app.use(express.json());
+app.use("/auth", authRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
+app.use("/users", usersRoute);
+
+
+app.listen(8000, ()=> {
+    connect()
+    console.log("connected to backend.");
+});
+
+
